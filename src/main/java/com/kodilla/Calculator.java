edf0b658bca6ae290067aa1211f1d31b089c9b0a
Kamil Biernat <k.biernat@yahoo.ca>
2022-02-28 11:45:42 +0100

    CalculatorAddSubtract

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,19 +1,5 @@
 package com.kodilla;
 
-class CalculatorAddSubtract {
-    int a;
-    int b;
-
-    public int sum(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-}
-
-
 public class Calculator {
     public static void main(String[] args) {
         CalculatorAddSubtract calc = new CalculatorAddSubtract();
---
+++ src/main/java/com/kodilla/CalculatorAddSubtract.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+class CalculatorAddSubtract {
+    int a;
+    int b;
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
