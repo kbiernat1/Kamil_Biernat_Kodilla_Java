e32ea15dc2cab75a32188b083d3ae37f45fbbb7c
Kamil Biernat <k.biernat@yahoo.ca>
2022-03-19 14:17:10 +0100

    7.4 (submodule: 'BigDecimal type') exercise SandStorage

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,33 +1,18 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        //I. Podejście funkcyjne – wyrażenia lambda
-        //         Processor processor = new Processor();
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));                    // [2]
 
-        //opcja 1: ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-        //         processor.execute(executeSaySomething);
-        //opcja 2: Executor task = () -> System.out.println("task to execute");
-        //opcja 3: processor.execute(() -> System.out.println("task to ex"));
-
-
-
-        //II. Wyrażenie lambda a metody z argumentami
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        /* expressionExecutor.executeExpression(3, 5 ,(a, b) -> a + b);
-        expressionExecutor.executeExpression(3, 5 ,(a, b) -> a - b);
-        expressionExecutor.executeExpression(3, 5 ,(a, b) -> a * b);
-        expressionExecutor.executeExpression(3, 5 ,(a, b) -> a / b); */
-
-
-
-        //III. Referencje do metod
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        System.out.println(theResultStringOfBooks);
     }
 }
