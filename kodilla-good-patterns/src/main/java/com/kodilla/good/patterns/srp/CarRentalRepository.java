e92d429be5d355bbf15583ef2db36cc904e46501
Kamil Biernat <k.biernat@yahoo.ca>
2022-04-02 15:49:20 +0200

    9.2 Example Rental (9 classes, 3 interfaces) - SRP

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/srp/CarRentalRepository.java
@@ -0,0 +1,12 @@
+package com.kodilla.good.patterns.srp;
+
+import java.time.LocalDateTime;
+
+public class CarRentalRepository implements RentalRepository{
+    public void createRental(User user, LocalDateTime from, LocalDateTime to) {
+    }
+
+    public void createCarRental(User user, LocalDateTime rentFrom, LocalDateTime rentTo) {
+
+    }
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/srp/MailService.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.srp;
+
+public class MailService implements InformationService{
+    public void inform(User user){
+    }
+
+    public void sendEmail(User user){
+
+    }
+}
