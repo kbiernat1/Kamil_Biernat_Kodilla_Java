1ef06abe40b18f2c376821a4863dfae5b94c111c
Kamil Biernat <k.biernat@yahoo.ca>
2022-04-07 17:36:03 +0200

    10.1 Added solution to ForumUser - what's the catch? O.O

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.forum;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+class ForumUserTestSuite {
+
+    @Test
+    void testGetUsername() {
+        //given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser forumUser = context.getBean(ForumUser.class);
+
+        //when
+        String name = forumUser.getUsername();
+
+        //then
+        Assertions.assertEquals("John Smith", name);
+
+    }
+}
