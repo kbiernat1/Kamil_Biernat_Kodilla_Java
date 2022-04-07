e658f67a8705cfb27a179e1a75243b0a830a10fd
Kamil Biernat <k.biernat@yahoo.ca>
2022-04-07 16:27:48 +0200

    10.1 first example for Spring (Bean, context, container) - interface Shape

---
+++ kodilla-spring/src/test/java/com/kodilla/kodillaspring/KodillaSpringApplicationTests.java
@@ -0,0 +1,44 @@
+package com.kodilla.kodillaspring;
+
+import com.kodilla.spring.shape.Circle;
+import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Triangle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+class KodillaSpringApplicationTests {
+
+    @Test
+    void testCircleLoadedIntoContainer() {
+
+        //given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Circle.class);
+
+        //when
+        String name = shape.getShapeName();
+
+        //then
+        Assertions.assertEquals("This is circle.", name);
+    }
+
+
+    @Test
+    void testTriangleLoadedIntoContainer() {
+
+        //given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Triangle.class);
+
+        //when
+        String name = shape.getShapeName();
+
+        //then
+        Assertions.assertEquals("This is triangle.", name);
+    }
+
+}
