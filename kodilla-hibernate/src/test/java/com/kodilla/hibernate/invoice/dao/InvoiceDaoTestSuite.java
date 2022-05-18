5008cd6866e28b7787e50724be8536b7eb465203
Kamil Biernat <k.biernat@yahoo.ca>
2022-05-18 13:32:10 +0200

    13.3 Exercise Invoice (1:N, N:1) - OneToMany, ManyToOne

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,49 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Test
+    void testInvoiceDaoSave() {
+        //G
+        Product product = new Product("TV");
+        Product product1 = new Product("Laptop");
+
+        Item item = new Item(product, new BigDecimal(3999), 5);
+        Item item1 = new Item(product, new BigDecimal(2999), 3);
+        Item item2 = new Item(product1, new BigDecimal(5999), 2);
+
+        Invoice invoice = new Invoice("IN/0642/2022");
+
+        item.setInvoice(invoice);
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+
+        invoice.getItems().add(item);
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+
+        //W
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //T
+        Assertions.assertNotEquals(0, id);
+
+        //Cleanup
+        invoiceDao.deleteById(id);
+    }
+}
