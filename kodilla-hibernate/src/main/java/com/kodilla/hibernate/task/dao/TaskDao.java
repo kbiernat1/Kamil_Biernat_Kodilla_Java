a08198528adffade9f0b8455758bcd0647a92f57
Kamil Biernat <k.biernat@yahoo.ca>
2022-05-18 15:06:06 +0200

    13.4 Example for HQL language & Named Queries

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -1,14 +1,29 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.time.Duration;
 import java.util.List;
 
 @Repository
 @Transactional
 public interface TaskDao extends CrudRepository<Task, Integer> {
     List<Task> findByDuration (int duration);
+
+    @Query
+    List<Task> retrieveLongTasks();
+
+    @Query
+    List<Task> retrieveShortTasks();
+
+    @Query
+    List<Task> retrieveTasksWithDurationLongerThan(@Param("DURATION") int duration);
+
+    @Query(nativeQuery = true)
+    List<Task> retrieveTasksWithEnoughTime();
 }
