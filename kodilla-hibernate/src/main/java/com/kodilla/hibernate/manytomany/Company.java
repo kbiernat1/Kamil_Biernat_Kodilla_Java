866b75bc6b0b3197a0c1eeb27b9945c69d22cdde
Kamil Biernat <k.biernat@yahoo.ca>
2022-05-18 16:41:21 +0200

    13.4 Exercise Company * Employees (FAILED)

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -5,6 +5,12 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+        name = "Company.retrieveFromCompanyNameByFirstThreeLetters",
+        query = "SELECT * FROM COMPANIES WHERE name.substring(0, 3) = :SUBSTRING(COMPANY_NAME, 1, 4)",
+        resultClass = Company.class
+)
+
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
