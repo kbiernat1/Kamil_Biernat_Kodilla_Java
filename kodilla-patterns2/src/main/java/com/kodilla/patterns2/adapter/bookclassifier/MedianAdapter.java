7e0fb7e5fa22a204cf2ddfa3d40374b6d7011d47
Kamil Biernat <k.biernat@yahoo.ca>
2022-07-04 09:34:13 +0200

    20.5 exercise MedianAdapter (added 2nd solution)

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
@@ -13,14 +13,25 @@ public class MedianAdapter extends MedianAdaptee implements Classifier {
 
     @Override
     public int publicationYearMedian(Set<Book> bookSet) {
-        Map<BookSignature, com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book> books = new HashMap<>();
+        //SOLUTION_1
 
-        for (Book bookA : bookSet) {
+        //Map<BookSignature, com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book> books = new HashMap<>();
+
+        /*for (Book bookA : bookSet) {
             com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book bookB =
                     new com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book(bookA.getAuthor(), bookA.getTitle(), bookA.getPublicationYear());
 
             books.put(new BookSignature(bookA.getSignature()), bookB);
-        }
+        }*/
+
+
+
+        //SOLUTION_2
+
+        Map<BookSignature, com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book> books = bookSet.stream()
+                .collect(Collectors.toMap(bookA -> new BookSignature(bookA.getSignature()), bookA ->
+                        new com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book(bookA.getAuthor(), bookA.getTitle(), bookA.getPublicationYear())));
+
         return medianPublicationYear(books);
     }
 }
