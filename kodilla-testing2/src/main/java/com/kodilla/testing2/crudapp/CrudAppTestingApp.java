dfca88b849df6d9d27457cb63ee49ed933334c84
Kamil Biernat <k.biernat@yahoo.ca>
2022-07-10 11:42:26 +0200

    23.2 exercise XPath-Relative

--- kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
@@ -9,7 +9,8 @@ import org.openqa.selenium.support.ui.Select;
 public class CrudAppTestingApp{
     public static final String XPATH_INPUT = "//html/body/main/section/form/fieldset/input";
     public static final String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea"; // [1]
-    public static final String XPATH_SELECT = "//select[1]";
+    public static final String XPATH_WAIT_FOR = "//select[1]";
+    public static final String XPATH_SELECT = "//div[contains(@class, \"task-container\")]/form/div/fieldset/select[1]";;
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
@@ -21,7 +22,7 @@ public class CrudAppTestingApp{
         WebElement textareaField = driver.findElement(By.xpath(XPATH_TEXTAREA));  // [2]
         textareaField.sendKeys("The robotic content");                            // [3]
 
-        while (!driver.findElement(By.xpath(XPATH_SELECT)).isDisplayed());
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed());
 
         WebElement selectCombo = driver.findElement(By.xpath(XPATH_SELECT));      // [2]
         Select selectBoard = new Select(selectCombo);                             // [3]
