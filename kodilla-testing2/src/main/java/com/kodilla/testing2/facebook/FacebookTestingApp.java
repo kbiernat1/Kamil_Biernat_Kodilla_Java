a1b35d277ec68051c4109b990ce007234fa8fbe0
Kamil Biernat <k.biernat@yahoo.ca>
2022-07-11 17:33:16 +0200

    23.3 exercise FacebookTestingApp (XPath-Relative) - 'while' replaced by 'Thread.sleep'

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -10,20 +10,19 @@ public class FacebookTestingApp {
     public static final String XPATH_SELECT_DAY = "//div[contains(@class, \"_5k_5\")]/span/span/select[1]";
     public static final String XPATH_SELECT_MONTH = "//div[contains(@class, \"_5k_5\")]/span/span/select[2]";
     public static final String XPATH_SELECT_YEAR = "//div[contains(@class, \"_5k_5\")]/span/span/select[3]";
-    public static final String XPATH_WAIT = "//select[1]";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://facebook.com/");
 
         driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[1]")).click();
         driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a")).click();
 
-        while (!driver.findElement(By.xpath(XPATH_WAIT)).isDisplayed());
+        Thread.sleep(1000);
 
         WebElement selectCombo1 = driver.findElement(By.xpath(XPATH_SELECT_DAY));
         Select selectBoard1 = new Select(selectCombo1);
-        selectBoard1.selectByIndex(7);
+        selectBoard1.selectByValue("1");
 
         WebElement selectCombo2 = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
         Select selectBoard2 = new Select(selectCombo2);
@@ -31,6 +30,6 @@ public class FacebookTestingApp {
 
         WebElement selectCombo3 = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
         Select selectBoard3 = new Select(selectCombo3);
-        selectBoard3.selectByIndex(5);
+        selectBoard3.selectByIndex(20);
     }
 }
